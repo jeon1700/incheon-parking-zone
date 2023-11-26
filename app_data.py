4d4ba77f381a5cdbf90019dd0380382ccec7f39c
jeon1700 <151480745+jeon1700@users.noreply.github.com>
2023-11-26 23:39:50 +0900

    main

--- app_data.py
+++ app_data.py
@@ -15,5 +15,19 @@ def data_app():
     else :
         st.text('')
 
+    st.text('최대 / 최소 데이터 확인하기')
+    column_list = df.columns[ 4 : ]
+    selected_columns = st.selectbox('컬럼을 선택하세요', column_list)
+
+    st.text(selected_columns + ' 컬럼의 최소값')
+    st.dataframe(df.loc[df[selected_columns] == df[selected_columns].min() , ])
+    st.text(selected_columns + ' 컬럼의 최대값')
+    st.dataframe(df.loc[df[selected_columns] == df[selected_columns].max() , ])
+
+    st.text(selected_columns + ' 컬럼의 히스토그램')
+
+    fig1 = plt.figure()
+    df[selected_columns].hist(bins = 20)
+    st.pyplot(fig1)    
 
 
