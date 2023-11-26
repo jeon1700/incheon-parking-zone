2e9dc3752aa3062cdfe80ce0e40c59f150b3964c
jeon1700 <151480745+jeon1700@users.noreply.github.com>
2023-11-26 21:43:20 +0900

    main

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 import streamlit as st
-
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sb
 
 def main():
-    pass
+    st.title('인천시 도로주정차 허용구간 안내')
+
+    menu = ['Home','','']
 
 
 if __name__ == '__main__' :
